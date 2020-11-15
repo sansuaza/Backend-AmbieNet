11a9efed4f5dd685933a5ac464b778188bc3b6a1
sansuaza <santi11_sz@hotmail.com>
2020-11-15 13:11:11 -0500

    Creación de los recursos base paralos permisos del admin con respecto a los posts

--- AmbieNet/posts/views/posts.py
+++ AmbieNet/posts/views/posts.py
@@ -11,6 +11,9 @@ from rest_framework import status, viewsets, mixins
 from AmbieNet.posts.models import Post
 from AmbieNet.users.models import User,Profile
 
+#Permissions
+from AmbieNet.posts.permissions import IsAdminUser
+
 #Serialzers
 from AmbieNet.posts.serializers import(
     PostModelSerializer,
@@ -18,19 +21,30 @@ from AmbieNet.posts.serializers import(
 )
 
 class PostViewSet(mixins.UpdateModelMixin,
-                mixins.ListModelMixin
-                ,viewsets.GenericViewSet):
+                mixins.ListModelMixin,
+                mixins.DestroyModelMixin,
+                viewsets.GenericViewSet):
     
     queryset = Post.objects.all()
     lookup_field = 'id'
+
+    def get_permissions(self):
+        """Assign the permissions based on action required."""
+        permissions = []
+        if self.action in ['delete']:
+            permissions = [isAdmin]
+        return [permissions() for permission in permissions]
+        
 
     def get_serializer_class(self):
         if (self.action in ['list', 'update']):
             return PostModelSerializer
         return PostCreateSerializer    
 
+
     @action(detail=False, methods=['post'])
     def postear(self,request, *args, **kwargs):
+        """Handle of create the posts."""
         serializer_class = self.get_serializer_class()
         serializer = serializer_class(data = request.data)
         serializer.is_valid(raise_exception=True)
@@ -38,4 +52,4 @@ class PostViewSet(mixins.UpdateModelMixin,
         data = PostModelSerializer(post).data
         return Response(data, status = status.HTTP_201_CREATED)
 
-            
+
