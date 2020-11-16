95ec50433288c2f324b8da6d15974ee385cea1a2
santiago-pat-mon <santiagopatinomontes@gmail.com>
2020-11-15 23:50:09 -0500

    se puso un [*] en ALLOWE_HOST

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,8 @@ from .base import env
 
 # Base
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['ambienetuq.tk'])
+ALLOWED_HOSTS=[*]
+#ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['ambienetuq.tk'])
 
 # Databases
 DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
