35acae454693b70036bed9251d97a1099f1b0d5b
santiago patiño <santiago.patino@agentemotor.com>
2021-10-27 22:48:06 -0500

    agregando config :D

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+"""Main URLs module."""
+
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+    # Django Admin
+    path(settings.ADMIN_URL, admin.site.urls),
+
+    path('', include(('AmbieNet.users.urls', 'users'), namespace = 'users')),
+    path('', include(('AmbieNet.posts.urls', 'posts'), namespace = 'posts')),
+    path('', include(('AmbieNet.users.urls', 'staffs'), namespace = 'staffs'))
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
